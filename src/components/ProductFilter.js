42f783e589b2b791664104d6536965e2b6337194
Zubenna <emelunna@gmail.com>
2021-07-21 07:57:15 +0100

    Corrected syntax error

--- src/components/ProductFilter.js
+++ src/components/ProductFilter.js
@@ -12,10 +12,10 @@ const ProductFilter = (props) => {
         <div className="flex flex-col w-auto">
           <h3 className="w-full md:w-40 mt-6 mb-4 text-center">Filter Products by Category</h3>
           <button type="button" className="filterButton h-8" id="all" onClick={handleClick}>All</button>
-          <button type="button" className="filterButton h-10" id="men-c" onClick={handleClick}>Men&aposs Clothing</button>
+          <button type="button" className="filterButton h-10" id="men-c" onClick={handleClick}>Men&apos;s Clothing</button>
           <button type="button" className="filterButton h-10" id="jewelry" onClick={handleClick}>Jewelery</button>
           <button type="button" className="filterButton h-10" id="electron" onClick={handleClick}>Electronics</button>
-          <button type="button" className="filterButton h-10" id="women-c" onClick={handleClick}>Women&aposs Clothing</button>
+          <button type="button" className="filterButton h-10" id="women-c" onClick={handleClick}>Women&apos;s Clothing</button>
           <button type="button" className="filterButton h-10" id="phone" onClick={handleClick}>Phones</button>
           <button type="button" className="filterButton h-10" id="furnit" onClick={handleClick}>Furniture</button>
         </div>
